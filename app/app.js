39bb788e80485394db886b1ed2044f57d0315304
a-stanisz <hello@adamstaniszewski.com>
2021-10-15 16:03:04 +0200

    Add a stub of system model

--- app/app.js
+++ app/app.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
-const PORT = process.env.NODE_DOCKER_PORT;
+
+app.use(
+  cors({
+    origin: 'http://localhost:8081',
+  })
+);
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+const db = require('./db/db');
+db.Sequelize.afterSync();
 
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  res.send('Hello ciuluu!');
 });
 
+const PORT = process.env.NODE_DOCKER_PORT || 8080;
 app.listen(PORT, () => {
   console.log(`App listening at localost:${PORT}`);
 });
--- app/db/db.js
+++ app/db/db.js
@@ -1,11 +1,11 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  database: process.env.MYSQLDB_DATABASE,
+  username: process.env.MYSQLDB_USER,
+  password: process.env.MYSQLDB_ROOT_PASSWORD,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.MYSQLDB_DOCKER_PORT,
   dialect: 'mysql',
   operatorsAliases: false,
   pool: {
@@ -17,3 +17,13 @@ const sequelize = new Sequelize({
 });
 
 module.exports = sequelize;
+
+
+MYSQLDB_USER=root
+MYSQLDB_ROOT_PASSWORD=123456
+MYSQLDB_DATABASE=test
+MYSQLDB_LOCAL_PORT=3307
+MYSQLDB_DOCKER_PORT=3306
+
+NODE_LOCAL_PORT=6868
+NODE_DOCKER_PORT=8080
